e20e718e1741e4d47d5361a7acc85f92a507800a
ChamiLamelas <chami.lamelas@gmail.com>
2022-07-31 00:58:59 -0400

    ran more experiments with new ranker setup

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,10 @@
 #include "random_ranker.h"
 #include "solver.h"
 #include "letter_ranker.h"
-#include "two_letter_ranker.h"
-#include "three_letter_ranker.h"
+#include "duplicate_ranker.h"
+#include "position_ranker.h"
+#include "word_frequency_ranker.h"
+#include "vowel_ranker.h"
 
 /*
 Main function - by default uses RunUserMode, can alternatively make
@@ -19,30 +21,57 @@ use of evaluation header.
 */
 int main()
 {
-    RandomRanker r1("RandomRanker(1)", 1);
-    LetterRanker r2;    
-    TwoLetterRanker r3; 
-    ThreeLetterRanker r4;
-    DuplicatePenaltyLetterRanker r5("DuplicatePenaltyLetterRanker(100)", 100);
-    DuplicatePenaltyTwoLetterRanker r6("DuplicatePenaltyTwoLetterRanker(100)", 100);
-    DuplicatePenaltyThreeLetterRanker r7("DuplicatePenaltyThreeLetterRanker(100)", 100);
-    RestrictedDuplicatePenaltyLetterRanker r8("RestrictedDuplicatePenaltyLetterRanker(100,2)", 100, 2);
-    RestrictedDuplicatePenaltyTwoLetterRanker r9("RestrictedDuplicatePenaltyTwoLetterRanker(100,2)", 100, 2);
-    RestrictedDuplicatePenaltyThreeLetterRanker r10("RestrictedDuplicatePenaltyThreeLetterRanker(100,2)", 100, 2);
-    RestrictedDuplicatePenaltyLetterRanker r11("RestrictedDuplicatePenaltyLetterRanker(100,3)", 100, 3);
-    RestrictedDuplicatePenaltyTwoLetterRanker r12("RestrictedDuplicatePenaltyTwoLetterRanker(100,3)", 100, 3);
-    RestrictedDuplicatePenaltyThreeLetterRanker r13("RestrictedDuplicatePenaltyThreeLetterRanker(100,3)", 100, 3);
+    std::string freq_fp{"data/five_letter_freq.csv"};
 
-    // 
+    RandomRanker r1("RandomRanker(1)", 1);
+    LetterRanker r2;
+    SubstringRanker r3("2LetterRanker", 2);
+    SubstringRanker r4("3LetterRanker", 3);
+    DuplicateRanker r5("DuplicateLetterRanker", &r2, 100);
+    DuplicateRanker r6("Duplicate2LetterRanker", &r3, 100);
+    DuplicateRanker r7("Duplicate3LetterRanker", &r4, 100);
+    DuplicateRanker r8("Restricted2DuplicateLetterRanker", &r2, 100, 2);
+    DuplicateRanker r9("Restricted2Duplicate2LetterRanker", &r3, 100, 2);
+    DuplicateRanker r10("Restricted2Duplicate3LetterRanker", &r4, 100, 2);
+    DuplicateRanker r11("Restricted3DuplicateLetterRanker", &r2, 100, 3);
+    DuplicateRanker r12("Restricted3Duplicate2LetterRanker", &r3, 100, 3);
+    DuplicateRanker r13("Restricted3Duplicate3LetterRanker", &r4, 100, 3);
+    PositionRanker r14("PositionRanker");
+    DuplicateRanker r15("DuplicatePositionRanker", &r14, 100);
+    DuplicateRanker r16("Restricted2DuplicatePositionRanker", &r14, 100, 2);
+    DuplicateRanker r17("Restricted3DuplicatePositionRanker", &r14, 100, 3);
+    WordFrequencyRanker r18("WordDuplicateLetterRanker", &r5, freq_fp);
+    WordFrequencyRanker r19("WordRestricted3DuplicateLetterRanker", &r11, freq_fp);
+    WordFrequencyRanker r20("WordDuplicate2LetterRanker", &r6, freq_fp);
+    WordFrequencyRanker r21("WordDuplicate3LetterRanker", &r7, freq_fp);
+    WordFrequencyRanker r22("WordRestricted3Duplicate2LetterRanker", &r12, freq_fp);
+    WordFrequencyRanker r23("WordRestricted3Duplicate3LetterRanker", &r13, freq_fp);
+    WordFrequencyRanker r24("WordDuplicatePositionRanker", &r15, freq_fp);
+    VowelRanker r25("VowelDuplicateLetterRanker", &r5, 100);
+    VowelRanker r26("VowelRestricted3DuplicateLetterRanker", &r11, 100);
+    VowelRanker r27("VowelDuplicate2LetterRanker", &r6, 100);
+    VowelRanker r28("VowelDuplicate3LetterRanker", &r7, 100);
+    VowelRanker r29("VowelRestricted3Duplicate2LetterRanker", &r12, 100);
+    VowelRanker r30("VowelRestricted3Duplicate3LetterRanker", &r13, 100);
+    VowelRanker r31("VowelDuplicatePositionRanker", &r15, 100, 2);
+    VowelRanker r32("Vowel2DuplicateLetterRanker", &r5, 100, 2);
+    VowelRanker r33("Vowel2Restricted3DuplicateLetterRanker", &r11, 100, 2);
+    VowelRanker r34("Vowel2Duplicate2LetterRanker", &r6, 100, 2);
+    VowelRanker r35("Vowel2Duplicate3LetterRanker", &r7, 100, 2);
+    VowelRanker r36("Vowel2Restricted3Duplicate2LetterRanker", &r12, 100, 2);
+    VowelRanker r37("Vowel2Restricted3Duplicate3LetterRanker", &r13, 100, 2);
+    VowelRanker r38("Vowel2DuplicatePositionRanker", &r15, 100, 2);
+
     std::vector<std::string> dictionary_fps{"data/dracos_github_words.txt", "data/medium_wordle_words_todate.txt"};
-    // &r1, &r2, &r3, &r4, &r5, &r6, &r7, &r8, &r9, &r10, &r11, &r12, &r13
-    std::vector<AbstractRanker *> rankers{&r1, &r2, &r3, &r4, &r5, &r6, &r7, &r8, &r9, &r10, &r11, &r12, &r13};
+    // &r1, &r2, &r3, &r4, &r5, &r6, &r7, &r8, &r9, &r10, &r11, &r12, &r13, &r14, &r15, &r16, &r17, &r18, &r19, &r20, &r21, &r22, &r23, &r24, &r25, &r26, &r27, &r28, &r29, &r30, &r31
+    // &r32, &r33, &r34, &r35, &r36, &r37, &r38
+    std::vector<AbstractRanker *> rankers{&r32, &r33, &r34, &r35, &r36, &r37, &r38};
 
     try
     {
-        WordleSolver solver(dictionary_fps[1], &r9, true);  
-        std::cout << "Evaluation result: " << Evaluate(solver, "lapse") << std::endl;
-        // RunUserMode(dictionary_fps[0], &lr, true);
+        // WordleSolver solver(dictionary_fps[1], &r9, true);
+        // std::cout << "Evaluation result: " << Evaluate(solver, "lapse") << std::endl;
+        RunUserMode(dictionary_fps[0], &r22);
         // GridEvaluate(dictionary_fps, rankers, "data/medium_wordle_words_todate.txt");
     }
     catch (const WordleSolverException &e)
