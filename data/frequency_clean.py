74cf36bef45cadbcb6bfc969c8df77a023ea36ba
ChamiLamelas <chami.lamelas@gmail.com>
2022-07-31 00:58:22 -0400

    fix up to 5 letter freq file

--- data/frequency_clean.py
+++ data/frequency_clean.py
@@ -5,8 +5,13 @@ Chami Lamelas
 Summer 2022
 """
 
+first = True
 with open("unigram_freq.csv", "r", encoding="utf-8") as rf, open("five_letter_freq.csv", "w+", encoding="utf-8") as wf:
     for line in rf:
         comma = line.index(',')
         if (comma == 5):
-            wf.write(line)
+            if not first:
+                wf.write("\n")
+            wf.write(line.rstrip())
+            if first:
+                first = False
